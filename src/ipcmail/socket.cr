6fada3fb451b44146b3c08023d6c48cf507015be
shpeckman <geelenrobin@proton.me>
2026-08-04 20:21:49 +0200

    style: fix inconsistent whitespace alignment

--- src/ipcmail/socket.cr
+++ src/ipcmail/socket.cr
@@ -28,7 +28,7 @@ module IPCMail
     def peer_credentials : Credentials
       {% if flag?(:linux) %}
         credentials = LibIPC::Ucred.new
-        size        = LibC::SocklenT.new(sizeof(LibIPC::Ucred))
+        size = LibC::SocklenT.new(sizeof(LibIPC::Ucred))
         result = LibC.getsockopt(@socket.fd, LibC::SOL_SOCKET, LibIPC::SO_PEERCRED,
           pointerof(credentials).as(Void*), pointerof(size))
         raise SystemError.new("getsockopt(SO_PEERCRED)") if result != 0
